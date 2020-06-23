39ae64c8193e1184ce36c752aaa7a13a15f99114
Amey Gupta <ameygupta9211@gmail.com>
2020-06-23 17:00:51 +0530

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ const partialsPath = path.join(__dirname, '../templates/partials')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
@@ -115,6 +117,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
