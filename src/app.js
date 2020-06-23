0a15537b35389efaa7cc0a5e8425a8c25aa94d4e
Amey Gupta <ameygupta9211@gmail.com>
2020-06-23 19:05:13 +0530

    changed index message

--- src/app.js
+++ src/app.js
@@ -32,14 +32,14 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about',{
-        title:'About Me',
+        title:'About',
         name:'Amey Gupta'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help',{
-        title:'Help Page',
+        title:'Help',
         message:'Hello, How may I help you',
         name:'Amey Gupta'
     })
